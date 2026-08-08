433f97bbd5ecaca7bfc61f3bd9862770ed6cfb23
csbahadurnagar123-lab <csbahadurnagar123@gmail.com>
2026-08-08 20:36:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from PyPDF import PdfReader
+from pypdf import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
